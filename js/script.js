81dc2b7ffcf30889ffa378888bdef85f6bc168e9
COPP <COPP@rce.rce>
2023-05-02 16:03:03 +0300

    firstcommit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const burgerBtn = document.querySelector('.burger-btn');
+const headerMenu = document.querySelector('.header-menu');
+
+burgerBtn.addEventListener('click', function (){
+    headerMenu.classList.toggle('active')
+    burgerBtn.classList.toggle('active')
+
+})
+
+console.log(burgerBtn)
